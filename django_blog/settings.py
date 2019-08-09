757aa3e734aff6654d4bb21b6ac58c894e1fc06c
Lgdev07 <luan_saopaulo06@hotmail.com>
2019-08-08 21:14:09 -0300

    [IMP] Adiciona versão web

--- django_blog/settings.py
+++ django_blog/settings.py
@@ -137,6 +137,6 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587 
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'gunsofgames466@gmail.com'
-EMAIL_HOST_PASSWORD = 'olawbvhrftdmmycw'
+EMAIL_HOST_USER = os.environ.get('DB_USER')
+EMAIL_HOST_PASSWORD = os.environ.get('DB_PASSWORD')
 
